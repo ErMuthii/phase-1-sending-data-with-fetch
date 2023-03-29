752e49877a632cfe77585500aaabf37ee2704faa
Mary Wanjiku <er.muthii@gmail.com>
2023-03-29 18:46:51 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
-// Add your code here
+function submitData(name,email){
+
+const configbody={name,email}
+
+const configdata={
+    method: 'POST',
+    header:{
+       "Content-Type":"application/json",
+       "Accept": "application/json"
+},
+    body: JSON.stringify(configbody)
+}
+
+return fetch("http://localhost:3000/users",configdata)
+.then(response=>response.json)
+.then(data=>{
+     console.log(data);
+     return data;
+})
+.catch(error=> {
+    console.error(error);
+    return error;
+
+})
+
+}
+
